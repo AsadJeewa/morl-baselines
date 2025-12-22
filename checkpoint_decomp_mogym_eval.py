bd73f1cee0a47c660213d5262de374085e0e672a
AsadJeewa <asad.jeewa@gmail.com>
2025-12-22 15:04:08 +0200

    feat: checkpoint CNN low level policies

--- checkpoint_decomp_mogym_eval.py
+++ checkpoint_decomp_mogym_eval.py
@@ -6,25 +6,31 @@ import gymnasium as gym
 import numpy as np
 from cleanrl_utils.utils import get_base_env
 from tqdm import tqdm
+from mo_gymnasium.envs.shapes_grid.shapes_grid import DIFFICULTY
 # --- Load checkpoint ---
 # checkpoint_path = "checkpoint/four-room-test/checkpoint_2480.pt"  # adjust path
 # checkpoint_path = "../cleanrl/model/shapes-grid/fin_moppo_env__shapes-grid-v0__moppo_decomp__1__1760003876/checkpoint_2160.pt"  # adjust path
 # checkpoint_path = "../cleanrl/model/shapes-grid/larger_grid_low_level__shapes-grid-v0__moppo_decomp__1__1760103414/checkpoint_2440.pt"  # adjust path
 # checkpoint_path = "../cleanrl/model/shapes-grid/toy_1__shapes-grid-v0__moppo_decomp__1__1763472383/checkpoint_120.pt"  # adjust path
 # checkpoint_path = "../cleanrl/model/shapes-grid/toy_2_penalty__shapes-grid-v0__moppo_decomp__1__1763473551/checkpoint_280.pt"  # adjust path
-checkpoint_path = "../cleanrl/model/shapes-grid/toy2_long__shapes-grid-v0__moppo_decomp__1__1763475033/checkpoint_1010.pt"  # adjust path
+# checkpoint_path = "../cleanrl/model/shapes-grid/toy2_long__shapes-grid-v0__moppo_decomp__1__1763475033/checkpoint_1010.pt"  # adjust path
+# checkpoint_path = "../cleanrl/model/shapes-grid/hard_low_level_pen__shapes-grid-v0__moppo_decomp__1__1763737176/checkpoint_2440.pt"  # adjust path
+# checkpoint_path = "../cleanrl/model/shapes-grid/SAVE/cnn_low_level__shapes-grid-v0__moppo_decomp__1__1766134932/checkpoint_70.pt"  # adjust path
+checkpoint_path = "../cleanrl/model/shapes-grid/SAVE/cnn_low_level_easy__shapes-grid-v0__moppo_decomp__1__1766138143/checkpoint_410.pt"  # adjust path
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 checkpoint = torch.load(checkpoint_path, map_location=device)
 
-
-
 # --- Environment factory ---
-def make_env(env_id, obj_idx=0, render=False, seed=None):
+def make_env(env_id, obj_idx=0, render=False, seed=None, difficulty=""):
     def thunk():
+        extra_kwargs = {}
+        if difficulty:
+            extra_kwargs["difficulty"] = DIFFICULTY[difficulty.upper()]
         if render:
-            env = mo_gym.make(env_id, render_mode="human")
+            env = mo_gym.make(env_id, render_mode="human", **extra_kwargs)
         else:
-            env = mo_gym.make(env_id)
+            env = mo_gym.make(env_id, **extra_kwargs)
         env = MORecordEpisodeStatistics(env, gamma=0.98)
         env = SingleRewardWrapper(env, obj_idx)
         if seed is not None:
@@ -35,7 +41,8 @@ def make_env(env_id, obj_idx=0, render=False, seed=None):
 
 idx = 0
 env_id = "shapes-grid-v0"
-env = gym.vector.SyncVectorEnv([make_env(env_id, idx)]) #expect list of callable of gym env
+difficulty="easy"
+env = gym.vector.SyncVectorEnv([make_env(env_id, idx, difficulty=difficulty)]) #expect list of callable of gym env
 
 # --- Create agent and load weights ---
 agent = Agent(env)
@@ -50,22 +57,21 @@ num_seeds = 3
 episodes_per_seed = 500
 all_rewards = []
 for seed in tqdm(range(num_seeds)):
-    env = gym.vector.SyncVectorEnv([make_env(env_id,idx,render=True,seed=seed) for _ in range(1)])  # single env
+    env = gym.vector.SyncVectorEnv([make_env(env_id,idx,render=True,seed=seed,difficulty=difficulty) for _ in range(1)])  # single env
     base_env = get_base_env(env.envs[0])
-    spec_obs = base_env.update_specialisation(idx+1)
+    base_env.set_specialisation(idx+1)
     for ep in tqdm(range(episodes_per_seed)):
         obs, _ = env.reset(seed=seed)
         done = False
         total_reward = 0.0
         while not done:
             # obs is already batched for vector env
-            obs_tensor = torch.tensor(spec_obs, dtype=torch.float32).to(device)
+            obs_tensor = torch.tensor(obs, dtype=torch.float32).to(device)
             with torch.no_grad():
                 action, _, _, _ = agent.get_action_and_value(obs_tensor)
             # Convert to numpy and step
             action_np = action.cpu().numpy()
             obs, reward, terminated, truncated, info = env.step(action_np)
-            spec_obs = base_env.get_spec_obs()
             total_reward += reward
             # Only one environment in batch, so index 0
             done = terminated[0] or truncated[0]
