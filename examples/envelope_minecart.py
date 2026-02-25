f455d23c380ac23ba644fdb3e43f666073f56391
AsadJeewa <asad.jeewa@gmail.com>
2026-02-25 12:44:09 +0200

    chore: better hyperparam for chpc

--- examples/envelope_minecart.py
+++ examples/envelope_minecart.py
@@ -1,7 +1,7 @@
 import mo_gymnasium as mo_gym
 import numpy as np
 from mo_gymnasium.wrappers import MORecordEpisodeStatistics
-
+#from mo_gymnasium.wrappers.vector import MOSyncVectorEnv
 from morl_baselines.multi_policy.envelope.envelope import Envelope
 
 
@@ -9,7 +9,7 @@ def main():
     def make_env():
         env = mo_gym.make("minecart-v0")
         env = MORecordEpisodeStatistics(env, gamma=0.98)
-        # env = mo_gym.LinearReward(env)
+        # env = MOSyncVectorEnv(env)
         return env
 
     env = make_env()
@@ -19,34 +19,34 @@ def main():
     agent = Envelope(
         env,
         max_grad_norm=1.0,#0.1 CHECK WAS TOO LOW
-        learning_rate=2e-4,# 3e-4 CHECK WAS LOW 
-        # gamma=0.98,
-        batch_size=32,
-        net_arch=[256, 256, 256, 256],
+        learning_rate=1e-4,# 3e-4 CHECK WAS LOW 
+        gamma=0.98,
+        batch_size=128,
+        net_arch=[128, 128],
         buffer_size=int(1.5e6),
         initial_epsilon=1.0,
         final_epsilon=0.05,
-        epsilon_decay_steps=100000,
-        # initial_homotopy_lambda=0.0,
-        # final_homotopy_lambda=1.0,
-        # homotopy_decay_steps=50000,
-        learning_starts=100,
+        epsilon_decay_steps=500000,
+        initial_homotopy_lambda=0.0,
+        final_homotopy_lambda=1.0,
+        homotopy_decay_steps=500000,
+        learning_starts=1000,
         envelope=True,
-        # gradient_updates=1,
-        # target_net_update_freq=1000,  # 1000,  # 500 reduce by gradient updates
-        tau=0.1,
+        gradient_updates=4,
+        target_net_update_freq=1000,  # 1000,  # 500 reduce by gradient updates
+        tau=0.05,
         log=True,
         project_name="MORL-Baselines",
         experiment_name="Envelope",
     )
 
     agent.train(
-        total_timesteps=100000,
+        total_timesteps=500000,
         total_episodes=None,
         weight=None,
         eval_env=eval_env,
-        ref_point=np.array([-1, -1, -200.0]),
-        # known_pareto_front=env.unwrapped.pareto_front(gamma=0.98),
+        ref_point=np.array([0, 0, -200.0]),
+        known_pareto_front=env.unwrapped.pareto_front(gamma=0.98),
         # num_eval_weights_for_front=100,
         # eval_freq=1000,
         # reset_num_timesteps=False,
