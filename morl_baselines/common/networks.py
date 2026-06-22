c2871e6583012d6f29d47d3a72ca75cb247b4de4
AsadJeewa <asad.jeewa@gmail.com>
2026-06-22 10:10:26 +0200

    chore: refactor CNNTorso

--- morl_baselines/common/networks.py
+++ morl_baselines/common/networks.py
@@ -170,24 +170,29 @@ class CNNTorso(nn.Module):
         super().__init__()
         c, h, w = input_shape
         self.conv1 = nn.Conv2d(c, 32, kernel_size=3, stride=1, padding=1) #greyscale
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.output_dim = output_dim
         
-        # Compute size after convs
-        def conv2d_size_out(size, kernel_size=3, stride=1, padding=1):
-            return (size + 2*padding - (kernel_size-1) -1)//stride +1
+        with torch.no_grad():
+            dummy = torch.zeros(1, c, h, w)
+            n_flatten = self._forward_conv(dummy).shape[1]
 
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 64
-        self.fc = nn.Linear(linear_input_size, output_dim)
+        self.fc = nn.Linear(n_flatten, output_dim)
 
     def forward(self, x):
+        # if x.dim() == 3:
+        #     x = x.unsqueeze(0)
         x = x.float()  # ensure float
+        if x.max() > 1:
+            x = x / 255.0
+        x = self._forward_conv(x)
+        x = F.relu(self.fc(x))
+        return x
+
+    def _forward_conv(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         x = x.view(x.size(0), -1)
-        x = F.relu(self.fc(x))
         return x
