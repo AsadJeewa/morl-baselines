29ba01a730ead505959ce63a7ce754055cd894d3
AsadJeewa <asad.jeewa@gmail.com>
2026-08-13 09:39:39 +0200

    feat: structured eval and controllability metrics

--- morl_eval.py
+++ morl_eval.py
@@ -1,41 +1,63 @@
+import pandas as pd
+import uuid
 import gymnasium as gym
 import mo_gymnasium as mo_gym
 from mo_gymnasium.wrappers import MORecordEpisodeStatistics
 import numpy as np
+# from cleanrl_utils.utils import get_base_env
 # from gymnasium.utils.play import play
-from cleanrl_utils.utils import get_base_env
 from morl_baselines.multi_policy.envelope.envelope import Envelope
 from morl_baselines.multi_policy.gpi_pd.gpi_pd import GPIPD
 from morl_baselines.common.weights import equally_spaced_weights
-from morl_baselines.common.plot_utils import plot_preferences #TODO pairwise
+from morl_baselines.common.plot_utils import plot_preferences, plot_correlations #TODO pairwise
 import torch
 import time
-from scipy.stats import pearsonr, spearmanr
 import matplotlib.pyplot as plt
 import seaborn as sns
 import re
 from pathlib import Path
 import fire
 import ast
+from gymnasium.wrappers import RecordVideo
+import os
+from scipy.stats import spearmanr
+from tqdm import tqdm
 
-def main(algo:str, seed: int = 0, env_id: str = "minecart-v0", num_neurons: int = 256, num_layers: int = 4, checkpoint_file: str = None, exp_note: str = ""):
+def main(algo:str, seed: int = 0, env_id: str = "minecart-v0", num_eval_episodes: int = 10,num_neurons: int = 256, num_layers: int = 4, checkpoint_file: str = None, exp_note: str = "", n_points=30, record_video: bool = False, right_angled: bool = True, mine_config: str = "mine_config.json"):
     RENDER_DELAY = 0
+    right_angled = str(right_angled).lower() == "true"
     net_arch = [int(num_neurons)] * int(num_layers)
     if checkpoint_file:
-        checkpoint_location = str(Path("examples/weights") / f"{Path(checkpoint_file).name}.tar")
-
-    algo = algo.lower()
+        checkpoint_location = checkpoint_location = f"examples/weights/{Path(checkpoint_file).name}.tar"
+    algo = (algo or "random").lower()
     is_gpi = "gpi" in algo
     is_env = "env" in algo
-    if not (is_gpi or is_env):
-        raise ValueError("Only Envelope and GPI are supported.")
-    
-    env = mo_gym.make(env_id)
+    is_random = not (is_gpi or is_env)    
+    kwargs = {}
+    if record_video:
+        kwargs["render_mode"] = "rgb_array"
+    if mine_config and "minecart" in env_id.lower():
+        kwargs["config"] = mine_config
+        
+    # 3. Create the environment safely
+    env = mo_gym.make(env_id, **kwargs)
     env = MORecordEpisodeStatistics(env, gamma=0.98)
+    if record_video:
+        video_dir = f"./videos/{env_id}/{algo}_{exp_note}"
+        os.makedirs(video_dir, exist_ok=True)
+
+        env = RecordVideo(
+            env,
+            video_folder=video_dir,
+            episode_trigger=lambda ep: True 
+            #  episode_trigger=lambda e: e % 1000 == 0
+        )
+
     obs, info = env.reset()
     done = False
     envelope = True
-    num_obj = get_base_env(env.env).reward_dim
+    # base_env = get_base_env(env.env)
+    num_obj = env.unwrapped.reward_dim
     device = "cuda" if torch.cuda.is_available() else "cpu"
     agent = None
     checkpoint = None
@@ -44,6 +66,7 @@ def main(algo:str, seed: int = 0, env_id: str = "minecart-v0", num_neurons: int 
     if checkpoint_file:
         checkpoint = torch.load(checkpoint_location,map_location=device)
         config = checkpoint.get("config", {})
+        training_seed = checkpoint.get("seed", seed)
     if is_env:
         net_arch = config.get("net_arch", net_arch)
         envelope = config.get("envelope", envelope)
@@ -85,82 +108,151 @@ def main(algo:str, seed: int = 0, env_id: str = "minecart-v0", num_neurons: int 
         agent.target_q_net.eval()
 
     num_eval_weights = 100
-    weights = equally_spaced_weights(dim=env.reward_dim, n=num_eval_weights, seed=seed+1000)
+    if not os.path.exists(f"results/{env_id}"):
+        os.makedirs(f"results/{env_id}", exist_ok=True)
+
+    weights = equally_spaced_weights(dim=num_obj, n=num_eval_weights, seed=seed+1000)
+    all_weights, all_returns = evaluate_policy(agent=agent, env=env, weights=weights, algo=algo, num_eval_episodes=num_eval_episodes)
+
+    set_id = str(uuid.uuid4())
+    print(set_id)
+    # plot_correlations(env, algo, all_weights, all_returns, exp_note=exp_note)
+    plot_preferences(set_id=set_id,seed=training_seed,agent=agent, env=env, algo=algo, n_points=n_points, exp_note=exp_note, right_angled=right_angled)
+    CO = compute_controllability(
+        all_weights,
+        all_returns
+    )
+    # Objective-wise controllability
+    objective_control = []
+
+    for d in range(num_obj):
+        preference_d = all_weights[:, d]
+        return_d = all_returns[:, d]
+
+        corr, _ = spearmanr(preference_d, return_d)
+        objective_control.append(corr)
+
+    objective_control = np.array(objective_control)
+
+    norm_returns = normalize_returns(all_returns)
+    local_sensitivity = compute_local_sensitivity(all_weights, all_returns) # norm_returns)
+
+    print("Preference controllability:", CO)
+    print("Objective controllability:", objective_control)
+    print("Local sensitivity:", local_sensitivity)
+
+    data = {
+    "set_id": set_id,
+    "training_seed": training_seed,
+    # "hypervolume": hv,
+    # "sparsity": sprs,
+    # "expected_utility": expected_utility(front, weights_list[mask]),
+    "preference_controllability": CO,
+    "local_sensitivity": local_sensitivity
+    }
+    # Add one column per objective
+    for d, score in enumerate(objective_control):
+        data[f"objective_controllability_{d}"] = score
+    df = pd.DataFrame([data])
+    filepath = f"results/{env_id}/metrics_{algo}_{exp_note}.csv"
+    df.to_csv(filepath, mode="a", index=False, header=not os.path.isfile(filepath))
+    env.close()
+
+def evaluate_policy(agent, env, weights, algo, num_eval_episodes):
+    algo_lower = algo.lower()
+    is_gpi = "gpi" in algo_lower
+    is_env = "env" in algo_lower
+
     all_weights = []
     all_returns = []
+    
+    i = 0
+    total_episodes = len(weights) * num_eval_episodes
+    pbar = tqdm(total=total_episodes, desc="Evaluating")
+    for w_np in weights:
+        print(i, " of ", len(weights))
+        i += 1
 
-    for idx, w_np in enumerate(weights):
+        episode_returns = []
 
-        obs, info = env.reset()
-        done = False
+        for episode in range(num_eval_episodes):
+            obs, _ = env.reset()
+            done = False
+            ep_return = np.zeros(env.unwrapped.reward_dim, dtype=np.float32)
 
-        ep_return = np.zeros(agent.reward_dim, dtype=np.float32)
+            while not done:
+                obs_tensor = torch.tensor(obs, dtype=torch.float32)
+                w_tensor = torch.tensor(w_np, dtype=torch.float32)
 
-        while not done:
+                with torch.no_grad():
+                    if is_gpi:
+                        action = agent.gpi_action(obs_tensor, w_tensor)
 
-            obs_tensor = torch.tensor(obs, dtype=torch.float32)
-            w_tensor = torch.tensor(w_np, dtype=torch.float32)
-            with torch.no_grad():
-                if is_gpi:
-                    action = agent.gpi_action(obs_tensor, w_tensor)
-                elif is_env:
-                    q = agent.q_net(obs_tensor.unsqueeze(0), w_tensor.unsqueeze(0))
-                    action = (q * w_tensor.unsqueeze(1)).sum(dim=2).argmax(dim=1).item()
-            obs, vec_reward, terminated, truncated, info = env.step(action)
-            time.sleep(RENDER_DELAY)
+                    elif is_env:
+                        q = agent.q_net(
+                            obs_tensor.unsqueeze(0),
+                            w_tensor.unsqueeze(0)
+                        )
+                        action = torch.einsum(
+                            "bar,r->ba", q, w_tensor
+                        ).argmax(dim=1).item()
 
-            ep_return += vec_reward
-            done = terminated or truncated
+                    else:
+                        action = env.action_space.sample()
+
+                obs, r, terminated, truncated, _ = env.step(action)
+                ep_return += r
+                done = terminated or truncated
+            pbar.update(1)
+            episode_returns.append(ep_return)
+
+        # Average the episodes for this preference
+        mean_return = np.mean(episode_returns, axis=0)
 
         all_weights.append(w_np)
-        all_returns.append(ep_return)
+        all_returns.append(mean_return)
+    pbar.close()
+    return np.array(all_weights), np.array(all_returns)
 
-        print(
-            f"{idx+1}/{num_eval_weights}",
-            "w =", np.round(w_np, 3),
-            "return =", np.round(ep_return, 3)
-        )
+def compute_controllability(weights, returns):
+    """
+    Cosine preference controllability.
+    Measures alignment between requested preferences and realised returns.
+    """
 
+    weights_norm = weights / (np.linalg.norm(weights, axis=1, keepdims=True) + 1e-8)
+    returns_norm = returns / (np.linalg.norm(returns, axis=1, keepdims=True) + 1e-8)
 
-    all_weights = np.array(all_weights)
-    all_returns = np.array(all_returns)
+    scores = np.sum(weights_norm * returns_norm, axis=1)
 
-    # flip cost objective (Minecart-specific)
-    # all_returns[:, 2] = -all_returns[:, 2]
+    return np.mean(scores)
 
-    print("\n=== Correlations ===")
+def compute_local_sensitivity(weights, returns):
+    sensitivities = []
 
-    for obj in range(agent.reward_dim):
+    for i in range(len(weights)):
+        distances = np.linalg.norm(weights - weights[i], axis=1)
+        distances[i] = np.inf
 
-        p_corr, _ = pearsonr(all_weights[:, obj], all_returns[:, obj])
-        s_corr, _ = spearmanr(all_weights[:, obj], all_returns[:, obj])
+        j = np.argmin(distances)
 
-        print(f"Obj {obj} | Pearson: {p_corr:.3f} | Spearman: {s_corr:.3f}")
+        dw = np.linalg.norm(weights[i] - weights[j])
+        dr = np.linalg.norm(returns[i] - returns[j])
+        # change in preference / change in behaviour​
+        sensitivities.append(dr / (dw + 1e-8))
+    return np.mean(sensitivities)
 
+def normalize_returns(returns, max_r=None, min_r=None):
+    """
+    Normalise each objective independently to [0,1].
+    returns: (N, D)
+    """
+    if min_r is None:
+        min_r = returns.min(axis=0)
+    if max_r is None:
+        max_r = returns.max(axis=0)
 
-    fig, axes = plt.subplots(1, num_obj, figsize=(5 * num_obj, 4))
-
-    for i in range(num_obj):
-
-        x = all_weights[:, i]
-        y = all_returns[:, i]
-
-        axes[i].scatter(x, y)
-
-        coeffs = np.polyfit(x, y, 1)
-        line = np.poly1d(coeffs)
-
-        xs = np.linspace(x.min(), x.max(), 100)
-        axes[i].plot(xs, line(xs))
-
-        axes[i].set_xlabel(f"w[{i}]")
-        axes[i].set_ylabel(f"r[{i}]")
-        axes[i].set_title(f"Obj {i}")
-
-    plt.tight_layout()
-    plt.savefig("weight_return_scatter_"+exp_note+".png")
-    plot_preferences(agent, algo,env, n_points=30, exp_note=exp_note)
-
+    return (returns - min_r) / (max_r - min_r + 1e-8)
 
 if __name__ == "__main__":
     fire.Fire(main)
