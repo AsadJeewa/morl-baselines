c7e3f8cff05d76bf84b131faac76e11f47b0d9e2
AsadJeewa <asad.jeewa@gmail.com>
2026-08-14 15:54:26 +0200

    chore: mo reacher hyperparam

--- examples/envelope_moreacher.py
+++ examples/envelope_moreacher.py
@@ -8,7 +8,7 @@ os.environ["MUJOCO_GL"] = "egl"
 from morl_baselines.multi_policy.envelope.envelope import Envelope
 from morl_baselines.common.weights import equally_spaced_weights, random_weights, extrema_weights, equally_spaced_train_and_eval_weights
 
-def main(total_timesteps: int=200000, exp_type: str = "default", wandb_mode: str = "online", log: bool = True, seed: int = 0, use_argmax_for_envelope: bool = False, use_train_weights_for_envelope: bool = False, exp_notes: str = "", mine_config: str = "mine_config.json"):
+def main(total_timesteps: int=500000, exp_type: str = "default", wandb_mode: str = "online", log: bool = True, seed: int = 0, use_argmax_for_envelope: bool = False, use_train_weights_for_envelope: bool = False, exp_notes: str = ""):
     log = str(log).lower() == "true"    
     def make_env():
         env = mo_gym.make("mo-reacher-v5")
@@ -37,22 +37,22 @@ def main(total_timesteps: int=200000, exp_type: str = "default", wandb_mode: str
         env,
         seed=seed,
         max_grad_norm=1.0,#0.1 CHECK WAS TOO LOW
-        learning_rate=3e-4,# 3e-4 CHECK WAS LOW 
+        learning_rate=1e-4,# 3e-4 CHECK WAS LOW 
         gamma=0.99,
-        batch_size=64,
+        batch_size=128,
         net_arch=[256, 256, 256, 256],
-        buffer_size=int(1e6),
+        buffer_size=int(5e5),
         initial_epsilon=1.0,
         final_epsilon=0.05,
-        epsilon_decay_steps=100000,
+        epsilon_decay_steps=50000,
         initial_homotopy_lambda=0,
         final_homotopy_lambda=1,
         # homotopy_decay_steps=0,
-        learning_starts=100,
+        learning_starts=5000,
         per=True,
         envelope=True,
         gradient_updates=1,
-        target_net_update_freq=1000,  # 1000,  # 500 reduce by gradient updates
+        target_net_update_freq=2000,  # 1000,  # 500 reduce by gradient updates
         tau=1,
         log=log,
         wandb_mode=wandb_mode,
--- examples/gpi_pd_moreacher.py
+++ examples/gpi_pd_moreacher.py
@@ -9,7 +9,7 @@ from mo_gymnasium.wrappers import MORecordEpisodeStatistics
 # from gymnasium.wrappers.record_video import RecordVideo
 
 
-def main(algo: str = "gpi-ls", gpi_pd: bool = False, g: int = 1, exp_type: str = None, wandb_mode: str = "online", log: bool = True, total_timesteps: int = 200000, timesteps_per_iter: int = 10000, seed: int = 0, exp_notes: str = "", mine_config: str = "mine_config.json"):
+def main(algo: str = "gpi-ls", gpi_pd: bool = False, g: int = 1, exp_type: str = None, wandb_mode: str = "online", log: bool = True, total_timesteps: int = 200000, timesteps_per_iter: int = 10000, seed: int = 0, exp_notes: str = ""):
     gpi_pd = str(gpi_pd).lower() == "true"
     log = str(log).lower() == "true"
     def make_env():
@@ -25,14 +25,14 @@ def main(algo: str = "gpi-ls", gpi_pd: bool = False, g: int = 1, exp_type: str =
         seed=seed,
         num_nets=2,
         # max_grad_norm=None,
-        learning_rate=3e-4,
+        learning_rate=1e-4,
         gamma=0.99,
         batch_size=128,
-        net_arch=[256, 256],
+        net_arch=[256, 256, 256, 256],
         buffer_size=int(1e6),
         initial_epsilon=1.0,
         final_epsilon=0.05,
-        epsilon_decay_steps=100000,
+        epsilon_decay_steps=50000,
         learning_starts=5000,
         alpha_per=0.6,
         min_priority=0.01,
@@ -40,7 +40,7 @@ def main(algo: str = "gpi-ls", gpi_pd: bool = False, g: int = 1, exp_type: str =
         gpi_pd=gpi_pd,
         use_gpi=True,
         gradient_updates=g,
-        target_net_update_freq=100,
+        target_net_update_freq=1000,
         tau=1,
         dyna=gpi_pd,
         dynamics_uncertainty_threshold=1.5,
